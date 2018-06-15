16cc3b4ee5adb2936cf305e56e8070fd0ede9e50
Dmitry Glinskiy <glinskiy.dmitry@gmail.com>
2018-06-15 12:08:11 +0300

    Schema iteration

--- src/models/charsheet.js
+++ src/models/charsheet.js
@@ -1,12 +1,53 @@
 var mongoose = require('mongoose');
 
+// The permitted SchemaTypes are: String, Number, Date, Boolean, Array, Map
 var schema = mongoose.Schema({
-	charsheet: {
+	characterName: {
 		type: String,
 		required: false
 	},
-	description: {
+	race: {
 		type: String,
+		required: false
+	},
+	class: {
+		type: String,
+		required: false
+	},
+	level: {
+		type: Number,
+		required: false
+	},
+	background: {
+		type: String,
+		required: false
+	},
+	alignment: {
+		type: String,
+		required: false
+	},
+	strength: {
+		type: Number,
+		required: false
+	},
+	dexterity: {
+		type: Number,
+		required: false
+	},
+	constitution: {
+		type: Number,
+		required: false
+	},
+	intelligence: {
+		type: Number,
+		required: false
+	},
+	wisdom: {
+		type: Number,
+		required: false
+	},
+	charisma: {
+		type: Number,
 		required: false
 	}
 });
